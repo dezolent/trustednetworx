dee15e22a84e8bc291f85b1eb18f9df6a356b0a0
Joseph Hill <dezolent@gmail.com>
2025-03-22 15:46:40 -0400

    Update PotsReplacement.tsx

--- src/pages/PotsReplacement.tsx
+++ src/pages/PotsReplacement.tsx
@@ -18,7 +18,7 @@ const PotsReplacement = () => {
           }}
         >
           {/* Overlay */}
-          <div className="absolute inset-0 bg-gradient-to-r from-blue-900/95 to-blue-600/90" />
+          <div className="absolute inset-0 bg-gradient-to-r from-black/95 to-black/50" />
         </div>
 
         <div className="relative z-10 w-full">
